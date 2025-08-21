5a01a5c1cc1af5596c126f17c026d92ca4beccc5
Riccardo Giovanni Rosso <riccardo.rosso@polito.it>
2025-08-21 15:35:26 +0200

    ripristino nuovo account

--- src/Login.js
+++ src/Login.js
@@ -95,7 +95,7 @@ export default function Login({ onLogin }) {
                 ))
               )}
             </div>
-            <button className={styles.loginButton} onClick={() => setShowNew(true)}>Nuovo</button>
+            <button className={styles.loginButton} onClick={() => setShowNew(true)}>Nuovo account</button>
           </>
         ) : (
           <form onSubmit={handleNewAccount}>
